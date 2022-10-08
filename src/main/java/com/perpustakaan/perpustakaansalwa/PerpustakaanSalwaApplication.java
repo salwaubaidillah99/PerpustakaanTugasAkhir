f7aaba6765277e81d9356ce88ccdf373d83fac49
salwaubaidillah99 <45916725+salwaubaidillah99@users.noreply.github.com>
2022-10-08 17:26:54 +0700

    firstcommit

---
+++ src/main/java/com/perpustakaan/perpustakaansalwa/PerpustakaanSalwaApplication.java
@@ -0,0 +1,13 @@
+package com.perpustakaan.perpustakaansalwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerpustakaanSalwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerpustakaanSalwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perpustakaan/perpustakaansalwa/PerpustakaanSalwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perpustakaan.perpustakaansalwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerpustakaanSalwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
